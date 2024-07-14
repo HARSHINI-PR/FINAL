15127fe377b91625e987b0526ee3d68e8edb8a94
HARSHINI-PR <harshiniprnkl@gmail.com>
2024-07-14 21:15:52 +0530

    project

---
+++ app-api-crud/src/main/java/app/api/app_api_crud/AppApiCrudApplication.java
@@ -0,0 +1,13 @@
+package app.api.app_api_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApiCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApiCrudApplication.class, args);
+	}
+
+}
